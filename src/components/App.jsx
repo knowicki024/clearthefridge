ce8e63ad67377463b019f6c4cd996c866197d63d
zaquari_andl <zaquariandl043@gmail.com>
2023-12-28 03:50:29 -0600

    added search bar with working functionality in react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Page from './Page';
 import Header from './Header';
+import Search from './Search'
 
 function App() {
+
+    const [ foodArr, setFoodArr] = useState([])
+
+    const [ searchVal, setSearchVal] = useState('')
+
+    const searchedFoodsArr = foodArr.filter(food => {
+        return food.name.toLowerCase().includes(searchVal.toLowerCase())
+    })
+
+    const handleSearch = (e) => {
+        setSearchVal(e.target.value)
+    }
+
     return (
         <div className='App'>
             <Header />
-            <Page />
+            <Search onSearch={handleSearch}/>
+            <Page foodArr={searchedFoodsArr}/>
         </div>
     )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Search() {
+function Search({onSearch}) {
     return (
         <div className='Search'>
-            <input className="search-bar" type="text"></input>
+            <input className="search-bar" type="text" placeholder='Search food...' onChange={onSearch}></input>
         </div>
     )
 }
