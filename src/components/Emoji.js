8d6a71aaa4a74e5f229ec5ba614de3f9bd1eaf9f
zaquari_andl <zaquariandl043@gmail.com>
2023-12-29 07:29:05 -0600

    edit

--- src/components/Emoji.js
+++ src/components/Emoji.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
+const ToolTip = ({ text }) => <div className="tooltip">{text}</div>  
 
-function Emoji({ emoji, returnFunction }) {
+function Emoji({ emoji, returnFunction, groceries }) {
 
-  function handleClick(event) {
-    returnFunction(event.target)
+  const [toolTip, setToolTip] = useState('');
+  
+  const [isHovered, setIsHovered] = useState(null);
+
+  function handleMouseEnter(groceries) {
+    setToolTip(groceries.tooltipText)
+    setIsHovered(groceries.id);
   }
 
-  return (
-    <button id={emoji.id} className="emoji-grid-button" onClick={handleClick}>
-      {emoji.image}
-    </button>
-  );
+  function handleMouseLeave() {
+    setToolTip('')
+    setIsHovered(null);
+  }
+
+  function handleClick(event) {
+    returnFunction(event.target);
+  }
+
+//   return (
+//     <button
+//       id={emoji.id}
+//       className={`emoji-grid-button ${isHovered ? "hovered" : ""}`}
+//       onClick={handleClick}
+//       onMouseEnter={handleMouseEnter}
+//       onMouseLeave={handleMouseLeave}
+//     >
+//       {emoji.image}
+//     </button>
+//   );
+
+return (
+  <div>
+      {groceries.map(groceries => (
+          <div 
+              key={groceries.id}
+              onMouseEnter={() => handleMouseEnter(groceries)}
+              onMouseLeave={handleMouseLeave}
+          >
+              {groceries.name}
+              {isHovered === groceries.id && <ToolTip text={toolTip} />}
+          </div>
+      ))}
+  </div>
+);
 }
+
 export default Emoji;
