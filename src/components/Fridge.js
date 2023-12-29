5a769829b62297bdc170552f7b0d1d4366c99bf1
zaquari_andl <zaquariandl043@gmail.com>
2023-12-29 02:20:59 -0600

    edit

--- src/components/Fridge.js
+++ src/components/Fridge.js
@@ -22,9 +22,9 @@ function handleClick(){
 
     return (
         <div className='Fridge'>
-            <img className='fridge-img' alt = "open fridge" src="https://media.discordapp.net/attachments/1187512873015062528/1187769590130933780/Image.png?ex=659817a1&is=6585a2a1&hm=73b4d0ab2ada80867b095feebecc69209d7c5d60db73c5a8ae00e2e0cca01790&=&format=webp&quality=lossless&width=620&height=662"/>
-            <ul className="food-cards">{foodCards}</ul>
-            <button onClick={handleClick}>Get groceries</button>
+            {/* <img className='fridge-img' alt = "open fridge" src="https://media.discordapp.net/attachments/1187512873015062528/1187769590130933780/Image.png?ex=659817a1&is=6585a2a1&hm=73b4d0ab2ada80867b095feebecc69209d7c5d60db73c5a8ae00e2e0cca01790&=&format=webp&quality=lossless&width=620&height=662"/> */}
+            <div className="food-cards">{foodCards}</div>
+            <button className="get-groceries" onClick={handleClick}>Get groceries</button>
         </div>
     )
 }
