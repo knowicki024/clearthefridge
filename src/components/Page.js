428710d5358e9b93132d2f28ac397c328c8187e5
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-02 14:07:52 -0500

    hooked up search component

--- src/components/Page.js
+++ src/components/Page.js
@@ -8,6 +8,7 @@ const API = " http://localhost:3000/groceries"
 function Page() {
 
     const [groceries, setGroceries] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
 
     useEffect(() => {
         fetch(API)
@@ -15,12 +16,20 @@ function Page() {
         .then(setGroceries)
      }, [])
 
+     const searchGroceries = groceries.filter(grocery => {
+        return grocery.name.toLowerCase().includes(searchTerm.toLowerCase())
+     })
+
+     const handleSearch = (e) => {
+        setSearchTerm(e.target.value)
+     }
+
     return (
         <div>
             <Header/>
-            <Search/>
+            <Search onSearch={handleSearch}/>
             <NewFoodForm />
-            <Fridge groceries={groceries}/>
+            <Fridge groceries={searchGroceries}/>
 
         </div>
     )
