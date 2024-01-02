55e5ea492f178ea4a1d9e39331accd338e8590d9
Steven Mentzer <stevenfmentzer@gmail.com>
2024-01-02 11:37:23 -0500

    index.css newfoodform fooditem

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,1 +1,37 @@
 import React from 'react';
+import { Grid, Card, GridColumn } from "semantic-ui-react";
+
+function FoodItem(){
+
+    let isBad = true; 
+
+    return (
+        <div className='food-display'>
+            <Grid columns={3}>
+                <Grid.Row>
+                    <Grid.Column width={7}>
+                        <div className='emoji-container'>
+                            <span className='emoji'>🍋</span>
+                        </div>
+                    </Grid.Column>
+                    <GridColumn width={1}></GridColumn>
+                    <Grid.Column width={7}>
+                        <Grid.Row>
+                            {isBad ? <h2>Food Name 🤢</h2> : <h2>Food Name</h2>}
+                            <p>Descpription</p>
+                            <Grid.Row>
+                            <p>Purchase Time: 'TIME'</p>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <p>Expiration Time: 'TIME'</p>
+                        </Grid.Row>
+                        </Grid.Row>
+                            <h1>TIMER</h1>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </div>
+    )
+}
+
+export default FoodItem;
