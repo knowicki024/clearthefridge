868c52de8f9595acf2a64bb1a0f6307aa5d5897d
Steven Mentzer <stevenfmentzer@gmail.com>
2024-01-03 11:54:49 -0500

    adding EDIT

--- src/components/FoodItem.js
+++ src/components/FoodItem.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { Grid } from "semantic-ui-react";
 import { useParams } from "react-router-dom"
 
-function FoodItem(){
-
+function FoodItem( {item} ){
     let { id } = useParams();
-
+    //Can Delete Later
     let isBad = true; 
+    //Delete Above
+    const [isEdit, setIsEdit] = useState(false)
+
+    const [formData, setFormData] = useState({
+        "note": " {item} "
+      });
+
+    function handleClick(){
+        setIsEdit(!isEdit)
+    }
+     const handleInputChange = (event) => {
+        setFormData({
+            ...formData,
+            ["note"]: event.target.value,
+        });
+        };
+
+    
 
     return (
         <>
@@ -20,6 +37,16 @@ function FoodItem(){
                     </Grid.Column>
                     <Grid.Column width={1}></Grid.Column>
                     <Grid.Column width={7}>
+                        {isEdit ?
+                        <form>
+                            <input
+                            type="text"
+                            name="note"
+                            value={formData.note}
+                            onChange={handleInputChange}/>
+                             <button type="submit">Submit</button>
+                        </form>
+                        :
                         <Grid.Row>
                             {isBad ? <h2>Food Name 🤢</h2> : <h2>Food Name</h2>}
                             <p>Description</p>
@@ -29,8 +56,9 @@ function FoodItem(){
                             <Grid.Row>
                                 <p>Expiration Time: 'TIME'</p>
                             </Grid.Row>
-                        </Grid.Row>
+                        </Grid.Row>}
                         <h1>TIMER</h1>
+                        <button onClick={handleClick}>Edit Note</button>
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
