56967c2360deef8c7eab2cc684d459e9c2a6b89b
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-02 21:45:24 -0500

    router rough draft

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 
-// import React from "react"
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import MainPage from './components/MainPage';
+import Header from './components/Header';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-// export default App;
+function App() {
+    return (
+        
+            <Router>
+                <Header />
+                <Routes>
+                    <Route
+                        path="/" 
+                        element={<MainPage/>}
+                        />
+                </Routes>
+            </Router>
+        
+    )
+    };
+
+
+
+export default App
